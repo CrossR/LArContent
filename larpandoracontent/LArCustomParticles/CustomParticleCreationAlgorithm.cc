c01c240e41717ea567cc18199b9094fd040c67b2
CrossR <r.cross@lancaster.ac.uk>
2019-11-04 16:53:26 +0000

    Write out the temporary metrics.
    Still need to work out how they are going to come together as one,
    but doing all for now to get an idea of the distributions.

--- larpandoracontent/LArCustomParticles/CustomParticleCreationAlgorithm.cc
+++ larpandoracontent/LArCustomParticles/CustomParticleCreationAlgorithm.cc
@@ -177,6 +177,13 @@ void CustomParticleCreationAlgorithm::plotMetrics(
     PANDORA_MONITORING_API(SetTreeVariable(this->GetPandora(), treeName.c_str(), "trackWasReconstructed", trackWasReconstructed));
     PANDORA_MONITORING_API(SetTreeVariable(this->GetPandora(), treeName.c_str(), "reconstructionState", reconstructionState));
 
+    PANDORA_MONITORING_API(SetTreeVariable(this->GetPandora(), treeName.c_str(), "recoUDisplacement", metricStruct.recoUDisplacement));
+    PANDORA_MONITORING_API(SetTreeVariable(this->GetPandora(), treeName.c_str(), "recoVDisplacement", metricStruct.recoVDisplacement));
+    PANDORA_MONITORING_API(SetTreeVariable(this->GetPandora(), treeName.c_str(), "recoWDisplacement", metricStruct.recoWDisplacement));
+    PANDORA_MONITORING_API(SetTreeVariable(this->GetPandora(), treeName.c_str(), "mcUDisplacement", metricStruct.mcUDisplacement));
+    PANDORA_MONITORING_API(SetTreeVariable(this->GetPandora(), treeName.c_str(), "mcUDisplacement", metricStruct.mcUDisplacement));
+    PANDORA_MONITORING_API(SetTreeVariable(this->GetPandora(), treeName.c_str(), "mcVDisplacement", metricStruct.mcVDisplacement));
+
     PANDORA_MONITORING_API(FillTree(this->GetPandora(), treeName.c_str()));
     PANDORA_MONITORING_API(SaveTree(this->GetPandora(), treeName.c_str(), fileName.c_str(), "RECREATE"));
     PANDORA_MONITORING_API(Delete(this->GetPandora()));
